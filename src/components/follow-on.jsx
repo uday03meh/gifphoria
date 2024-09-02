9bd01d2569bb1a7eaa606f859751b62f21937b31
uday03meh <udaymehtani@gmail.com>
2024-09-02 23:17:18 +0530

    first commit

---
+++ src/components/follow-on.jsx
@@ -0,0 +1,13 @@
+import {FaInstagram, FaXTwitter, FaYoutube} from "react-icons/fa6";
+
+const FollowOn = () => {
+  return (
+    <div
+      className="faded-text pt-2" //custom - faded-text
+    >
+      
+    </div>
+  );
+};
+
+export default FollowOn;
